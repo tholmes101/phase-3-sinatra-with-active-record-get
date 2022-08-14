f9dbc5f67714f88aaa2821dde4ed2519bffd8fa2
Terence Holmes <piece4ulmind@gmail.com>
2022-08-14 19:33:35 -0400

    Completed assignment

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < Sinatra::Base
+  set :default_content_type, 'application/json'
 
-  get '/' do
-    { message: "Hello world" }.to_json
+  get '/games' do
+    # get all the games from the database
+    games = Game.all
+    # return a JSON response with an array of all the game data
+    games.to_json
+  end
+
+  get '/games/:id' do
+    game = Game.find(params[:id])
+
+    # include associated reviews in the JSON response
+    game.to_json(only: [:id, :title, :genre, :price], include: {
+      reviews: { only: [:comment, :score], include: {
+        user: { only: [:name] }
+      } }
+    })
   end
 
 end
